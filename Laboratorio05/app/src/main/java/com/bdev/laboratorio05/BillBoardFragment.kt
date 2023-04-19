b53ecfaada67a10bab5e2324b9d58e53936d6be8
FFigueroa17 <00019921@uca.edu.sv>
2023-04-19 15:18:47 -0600

    :memo: Laboratorio05

---
+++ Laboratorio05/app/src/main/java/com/bdev/laboratorio05/BillBoardFragment.kt
@@ -0,0 +1,40 @@
+package com.bdev.laboratorio05
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+import androidx.navigation.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class BillBoardFragment : Fragment() {
+
+    private lateinit var  actionNavToCreateMovie: FloatingActionButton
+    private lateinit var  actionCardViewToDescription: CardView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bill_board, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        actionNavToCreateMovie= view.findViewById(R.id.actionNavToCreateMovie)
+        actionCardViewToDescription = view.findViewById(R.id.actionCardViewToDescription)
+
+        actionNavToCreateMovie.setOnClickListener {
+            it.findNavController().navigate(R.id.action_billBoardFragment_to_createMovieFragment)
+        }
+
+        actionCardViewToDescription.setOnClickListener{
+            it.findNavController().navigate(R.id.action_billBoardFragment_to_movieFragment)
+        }
+    }
+
+}
